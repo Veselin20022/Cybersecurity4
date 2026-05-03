0873de06604b9622bc247f4346403e12df5b0470
Veselin20022 <veselin20022st@gmail.com>
2026-05-03 18:06:44 +0300

    Refactor OpenSSL script for better readability

--- OpenSSL_MD5_and_SHA-256.sh
+++ OpenSSL_MD5_and_SHA-256.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "=== MD5 ==="
+echo "MD5"
 openssl md5 file1.pdf
 openssl md5 file2.pdf
 
 echo ""
-echo "=== SHA-256 ==="
+echo "SHA-256"
 openssl sha256 file1.pdf
 openssl sha256 file2.pdf
